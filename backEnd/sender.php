2b82f9037fc5a9744cc48081ecc2f3624262a002
Anish <6275anioshroy@gmail.com>
2020-08-01 14:59:57 +0530

    emoji problem solved

--- backEnd/sender.php
+++ backEnd/sender.php
@@ -1,6 +1,14 @@
 <?php
     include '../dbManager.php';
+    $msg = $_POST['msg'];
+    $times = substr_count($msg,"alt=");
+    if($times > 0){
+        for($p = 1; $p <= $times ; $p++){
+            $i = strpos($msg, 'alt=');
+            $msg = substr_replace($msg, '', $i, 12); 
+        }
+    }
     $sql = "INSERT INTO `message` (`slno`, `sID`, `rID`, `type`, `message`, `time`) VALUES 
-                (NULL, '{$_POST['org']}', '{$_POST['dest']}', '{$_POST['kind']}', '{$_POST['msg']}', '{$_POST['time']}');";
+                (NULL, '{$_POST['org']}', '{$_POST['dest']}', '{$_POST['kind']}', '{$msg}', '{$_POST['time']}');";
     mysqli_query($conn, $sql);
 ?>
